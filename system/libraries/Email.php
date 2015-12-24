4a44c5e36b18c859f68c48c684af0b395cf80e06
Joko Purnomo A <jokopurnomoa@gmail.com>
2015-12-24 23:45:44 +0700

    modify email library

--- system/libraries/Email.php
+++ system/libraries/Email.php
@@ -12,73 +12,80 @@ class Email {
     public static function init(){
         require 'PHPMailer/PHPMailerAutoload.php';
         self::$phpMailer = new PHPMailer();
+        self::$phpMailer->isMail();
     }
 
-    public static function isSMTP(){
-        return self::$phpMailer->isSMTP();
+    public static function smtp($status){
+        if($status === true){
+            return self::$phpMailer->isSMTP();
+        }
+        return false;
     }
 
-    public static function setHost($host){
+    public static function host($host){
         self::$phpMailer->Host = $host;
     }
 
-    public static function setSMTPAuth($smtp_auth){
+    public static function SMTPAuth($smtp_auth){
         self::$phpMailer->SMTPAuth = $smtp_auth;
     }
 
-    public static function setUsername($username){
+    public static function username($username){
         self::$phpMailer->Username = $username;
     }
 
-    public static function setPassword($password){
+    public static function password($password){
         self::$phpMailer->Password = $password;
     }
 
-    public static function setSMTPSecure($smtp_secure){
+    public static function SMTPSecure($smtp_secure){
         self::$phpMailer->SMTPSecure = $smtp_secure;
     }
 
-    public static function setPort($port){
+    public static function port($port){
         self::$phpMailer->Port = $port;
     }
 
-    public static function setFrom($email, $name){
+    public static function from($email, $name){
         return self::$phpMailer->setFrom($email, $name);
     }
 
-    public static function addAddress($email, $name = ''){
+    public static function to($email, $name = ''){
         return self::$phpMailer->addAddress($email, $name);
     }
 
-    public static function addReplyTo($email, $name){
+    public static function replyTo($email, $name){
         return self::$phpMailer->addReplyTo($email, $name);
     }
 
-    public static function addCC($email){
+    public static function cc($email){
         return self::$phpMailer->addCC($email);
     }
 
-    public static function addBCC($email){
+    public static function bcc($email){
         return self::$phpMailer->addBCC($email);
     }
 
-    public static function addAttachment($file){
+    public static function attachment($file){
         return self::$phpMailer->addAttachment($file);
     }
 
-    public static function isHTML(){
-        return self::$phpMailer->isHTML();
+    public static function html($option){
+        if($option === true){
+            return self::$phpMailer->isHTML();
+        }
+        return false;
     }
 
-    public static function setSubject($subject){
+    public static function subject($subject){
          self::$phpMailer->Subject = $subject;
     }
 
-    public static function setBody($body){
+    public static function message($body){
         self::$phpMailer->Body = $body;
     }
 
-    public static function setAltBody($alt_body){
+    public static function altBody($alt_body){
         self::$phpMailer->AltBody = $alt_body;
     }
 
