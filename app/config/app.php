527a6fe8d3c94048ca83e159fbda75c13272576f
jokopurnomoa <jokopurnomoa@gmail.com>
2016-03-21 15:00:01 +0700

    add config file documentation

--- app/config/app.php
+++ app/config/app.php
@@ -5,7 +5,26 @@
  *
  */
 
+/**
+ * Application Base Url
+ *
+ * Example:
+ * $config['base_url'] = 'example.com';
+ *
+ */
+
 $config['base_url'] = '';
+
+/**
+ * Session Config
+ *
+ * Example:
+ * $config['session']['name']   = 'ELIPS_PHP_SESS';     // session name
+ * $config['session']['driver'] = 'file';               // session driver (file)
+ * $config['session']['expire'] = 7200;                 // session expire time
+ * $config['session']['match_ip'] = true;               // session match ip address (true | false)
+ * $config['session']['max_size'] = 1048576;            // session max file size (if use file driver)
+ */
 
 $config['session']['name']   = 'ELIPS_PHP_SESS';
 $config['session']['driver'] = 'file';
@@ -13,7 +32,25 @@ $config['session']['expire'] = 7200;
 $config['session']['match_ip'] = true;
 $config['session']['max_size'] = 1048576;
 
+/**
+ * Encryption Key
+ *
+ * Used by Encryption library, Session library, Cache library
+ *
+ * Example:
+ * $config['encryption_key'] = 'MY SECRET KEY';
+ */
+
 $config['encryption_key'] = '427ba3403980c1b3471f3fe226b1b92a896063e223';
+
+/**
+ * Cache Config
+ *
+ * Example:
+ * $config['cache']['driver'] = 'file';     // cache driver (file | apc)
+ * $config['cache']['active'] = false;      // is cache active (true | false)
+ * $config['cache']['encrypt'] = false;     // encrypt cache data (true | false)
+ */
 
 $config['cache']['driver'] = 'file';
 $config['cache']['active'] = false;
--- app/config/route.php
+++ app/config/route.php
@@ -7,6 +7,14 @@
  *
  */
 
+/**
+ * Example:
+ * $route['default_controller'] = 'Welcome';    // default application controller (first controller called)
+ * $route['root_controller'] = 'Base';          // root controller
+ * $route['404'] = 'Error404';                          // page not found controller
+ * $route['method_separator'] = '-';             // method separator to call method in url ('_' | '-' | default is '_')
+ */
+
 $route['default_controller'] = 'Welcome';
 $route['root_controller'] = 'Base';
 $route['404'] = '';
