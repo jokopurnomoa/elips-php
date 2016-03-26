250277094f437caf1e53508199141925bc2c40d7
jokopurnomoa <jokopurnomoa@gmail.com>
2016-03-26 22:45:08 +0700

    modify app config & route config

--- app/config/app.php
+++ app/config/app.php
@@ -5,53 +5,58 @@
  *
  */
 
-/**
- * Application Base Url
- *
- * Example:
- * $config['base_url'] = 'example.com';
- *
- */
+$config = array(
+    /**
+     * Application Base Url
+     *
+     * Example:
+     * $config['base_url'] = 'example.com';
+     *
+     */
 
-$config['base_url'] = '';
+    'base_url' => '',
 
-/**
- * Session Config
- *
- * Example:
- * $config['session']['name']   = 'ELIPS_PHP_SESS';     // session name
- * $config['session']['driver'] = 'file';               // session driver (file)
- * $config['session']['expire'] = 7200;                 // session expire time
- * $config['session']['match_ip'] = true;               // session match ip address (true | false)
- * $config['session']['max_size'] = 1048576;            // session max file size (if use file driver)
- */
+    /**
+     * Session Config
+     *
+     * name         session name                                // 'ELIPS_PHP_SESS'
+     * driver       session driver (file)                       // 'file'
+     * expire       session expire time                         // 7200
+     * match_ip     session match ip address (true | false)     // true
+     * max_size     session max file size (if use file driver)  // 1048576
+     */
 
-$config['session']['name']   = 'ELIPS_PHP_SESS';
-$config['session']['driver'] = 'file';
-$config['session']['expire'] = 7200;
-$config['session']['match_ip'] = true;
-$config['session']['max_size'] = 1048576;
+    'session' => array(
+        'name'      => 'ELIPS_PHP_SESS',
+        'driver'    => 'file',
+        'expire'    => 7200,
+        'match_ip'  => true,
+        'max_size'  => 1048576
+    ),
 
-/**
- * Encryption Key
- *
- * Used by Encryption library, Session library, Cache library
- *
- * Example:
- * $config['encryption_key'] = 'MY SECRET KEY';
- */
+    /**
+     * Encryption Key
+     *
+     * Used by Encryption library, Session library, Cache library
+     *
+     * Example:
+     * $config['encryption_key'] = 'MY SECRET KEY';
+     */
 
-$config['encryption_key'] = '427ba3403980c1b3471f3fe226b1b92a896063e223';
+    'encryption_key' => '427ba3403980c1b3471f3fe226b1b92a896063e223',
 
-/**
- * Cache Config
- *
- * Example:
- * $config['cache']['driver'] = 'file';     // cache driver (file | apc)
- * $config['cache']['active'] = false;      // is cache active (true | false)
- * $config['cache']['encrypt'] = false;     // encrypt cache data (true | false)
- */
+    /**
+     * Cache Config
+     *
+     * driver     cache driver (file | apc)             // 'file'
+     * active     is cache active (true | false)        // false
+     * encrypt    encrypt cache data (true | false)     // false
+     */
 
-$config['cache']['driver'] = 'file';
-$config['cache']['active'] = false;
-$config['cache']['encrypt'] = false;
+    'cache' => array(
+        'driver'    => 'file',
+        'active'    => false,
+        'encrypt'   => false
+    )
+
+);
--- app/config/route.php
+++ app/config/route.php
@@ -8,14 +8,20 @@
  */
 
 /**
- * Example:
- * $route['default_controller'] = 'Welcome';    // default application controller (first controller called)
- * $route['root_controller'] = 'Base';          // root controller
- * $route['404'] = 'Error404';                  // page not found controller
- * $route['method_separator'] = '-';            // method separator to call method in url ('_' | '-' | default is '_')
+ * default_controller       // default application controller (first controller called)
+ * root_controller          // root controller
+ * 404                      // page not found controller
+ * method_separator         // method separator to call method in url ('_' | '-' | default is '_')
  */
 
-$route['default_controller'] = 'Welcome';
-$route['root_controller'] = 'Base';
-$route['404'] = '';
-$route['method_separator'] = '';
+$route = array(
+
+    'default_controller'        => 'Welcome',
+
+    'root_controller'           => 'Base',
+
+    '404'                       => '',
+
+    'method_separator'          => '',
+
+);
