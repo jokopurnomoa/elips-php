4526e1bd540ef741a485bf39d7f530cf049e0d43
jokopurnomoa <jokopurnomoa@gmail.com>
2016-03-30 01:03:50 +0700

    update error helper style

--- framework/helpers/error.php
+++ framework/helpers/error.php
@@ -12,7 +12,7 @@
  * @param $message
  */
 function error_dump($message){
-    echo '<pre style="font-family: \'Courier New\', Monospace;padding: 30px;border: 1px solid #eee">';
+    echo '<pre style="font-family: \'Courier New\', Monospace;padding: 30px;border: 1px solid #eee;margin: 50px 20px;border-radius: 5px">';
     print_r($message);
     echo '</pre>';
 }
