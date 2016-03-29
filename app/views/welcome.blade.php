a088e451cbdd8285e3103444c50890f6af6d15cd
jokopurnomoa <jokopurnomoa@gmail.com>
2016-03-30 00:53:13 +0700

    update welcome view

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -18,7 +18,7 @@
         .footer{
             width: 100%;
             text-align: center;
-            font-weight: 300;
+            font-weight: 500;
             font-size: 13px;
             position: absolute;
             bottom: 10px;
